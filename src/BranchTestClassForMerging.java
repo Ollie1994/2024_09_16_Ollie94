dee310e330798424ee3438ebd34debeaeb62eda2
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-16 15:08:55 +0200

    taBort?

--- src/BranchTestClassForMerging.java
+++
@@ -1,5 +0,0 @@
-public class BranchTestClassForMerging {
-    public static void main(String[] args) {
-        System.out.println("testFörMerging");
-    }
-}
--- src/TestBranch_2.java
+++
@@ -1,5 +0,0 @@
-public class TestBranch_2 {
-    public static void main(String[] args) {
-        System.out.println("testAvBranchOMERge2");
-    }
-}
