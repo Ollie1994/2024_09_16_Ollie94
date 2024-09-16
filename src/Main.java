767a3ae5191a3de83202b341a5ef99769f360794
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-16 13:58:36 +0200

    forktest

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int score = calculateScore("Helena", 200);
         System.out.println(score);
-
+        System.out.println("ForkTest");
         int newScore = calculateScore(300);
         System.out.println(newScore);
 
