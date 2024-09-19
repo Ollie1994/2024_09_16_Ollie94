48e13cddf103181c7cd841274b7aa7058547d578
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-19 15:42:38 +0200

    påbörjantilldynamsik med scanner input

--- src/hotel/Main.java
+++ src/hotel/Main.java
@@ -1,46 +1,43 @@
 package hotel;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // skapa ett objekt av Bookingservice så vi kan komma åt alla metoder
+        Scanner sc = new Scanner(System.in);
         BookingService bookingService = new BookingService();
-
+//--------------------------RAW DATA---------------------------------------------------------
         Customer customer1   = new Customer("Robert Nilsson", "0704562345");
-
         StandardRoom room1 = new StandardRoom("101", 2, 1000, true);
         StandardRoom room2 = new StandardRoom("102", 2, 1000, true);
         StandardRoom room3 = new StandardRoom("103", 2, 1000, true);
-
         Suite suite1 = new Suite("201", 4, 5000, true);
 
+//-------------------------ADDING TO HOTEL AND REMOVING-----------------------------------
         bookingService.addRoom(room1);
+        bookingService.addRoom(room2);
+        bookingService.addRoom(room3);
         bookingService.addRoom(suite1);
         bookingService.addCustomer(customer1);
 
+//------------------------------------Input--------------------------------------
+        while(true) {
+            System.out.println("Menu");
+            System.out.println("1. Add/Remove room");
+            System.out.println("2. Add/Remove suite");
+            System.out.println("3. Add/Remove customer");
+            System.out.println("4. Exit");
+            int menuChoice = sc.nextInt();
+            switch(menuChoice) {
+                case 1:
+            }
+        }
 
-        System.out.println("---------------------");
 
-        System.out.println("Tillgängliga rum: ");
-        bookingService.displayAvailableRooms();
-        System.out.println("---------------------");
 
-        System.out.println("Bokar rum 101:");
-        bookingService.bookRoom("101", 2);
-        System.out.println("---------------------");
 
-        System.out.println("Försöker boka rum 101 igen:");
-        bookingService.bookRoom("101", 4);
-        System.out.println("---------------------");
 
-        System.out.println("Avbokar rum 101");
-        bookingService.cancelBooking("101");
-        System.out.println("---------------------");
-        System.out.println("Bokar rum 201:");
-        bookingService.bookRoom("201", 4);
-        System.out.println("---------------------");
-
-        System.out.println("Tillgängliga rum efter alla tester");
-        bookingService.displayAvailableRooms();
 
     }
 }
